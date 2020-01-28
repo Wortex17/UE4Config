3c5e73ec6d4d7bbee141fd142c4068765d1cde01
Patrick Michael Hopf <dragonia@gmx.at>
2020-01-28 21:50:59 +0100

    Project Scaffolding

---
+++ UE4Config.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UE4Config.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UE4Config.Tests")]
+[assembly: AssemblyCopyright("Copyright © Patrick Michael Hopf 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6001a205-97eb-4c5f-87f7-b91c4c41a6fc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UE4Config.Tests/SanityTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace UE4Config.Tests
+{
+    [TestFixture]
+    class SanityTests
+    {
+        public void When_CheckingTestSanity()
+        {
+            Assert.That("Sanity", Is.EqualTo("Sanity"));
+            Assert.That(true, Is.True);
+            Assert.That(false, Is.False);
+        }
+    }
+}
