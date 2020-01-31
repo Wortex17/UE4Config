aed32bf9e49f8c90e8d78f59101ecc7707e7acd8
Patrick Michael Hopf <dragonia@gmx.at>
2020-01-31 21:31:06 +0100

    Reduce complexity of TextLine

--- UE4Config/Parsing/TextLine.cs
+++ UE4Config/Parsing/TextLine.cs
@@ -20,33 +20,9 @@ namespace UE4Config.Parsing
             return Content + LineEnding.AsString();
         }
 
-        public override bool Equals(object obj)
-        {
-            if (obj is string)
-            {
-                return String.Equals(Content, obj);
-            }
-            return base.Equals(obj);
-        }
-
-        public static implicit operator string(TextLine line)
-        {
-            return line.Content;
-        }
-
         public static implicit operator TextLine(string content)
         {
             return new TextLine(content);
-        }
-
-        public static bool operator ==(TextLine line, string str)
-        {
-            return line.Equals(str);
-        }
-
-        public static bool operator !=(TextLine line, string str)
-        {
-            return !line.Equals(str);
         }
     }
 }
