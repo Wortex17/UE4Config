8696be96e6417ca2552642f124bd795e165db1be
Patrick Michael Hopf <dragonia@gmx.at>
2020-03-04 17:04:20 +0100

    Update sample tests

--- UE4Config.Tests/SampleTests.cs
+++ UE4Config.Tests/SampleTests.cs
@@ -17,12 +17,15 @@ namespace UE4Config.Tests
         {
             //Create a new config with a name for identification
             var config = new ConfigIni("DefaultGame");
+
             //Load the configs contents from a file
             config.Read(File.OpenText(TestUtils.GetTestDataPath("MockProject/Config/DefaultGame.ini")));
+
             //Evaluate the values and put them into a list. Should the property only have a single value, the list will have a single element.
             //Should the property not exist or should all its values have been deleted via config, the list will be empty.
             var values = new List<string>();
             config.EvaluatePropertyValues("/Script/EngineSettings.GeneralProjectSettings", "ProjectID", values);
+
             Assert.That(values, Is.EquivalentTo(new[]{"3F9D696D4363312194B0ECB2671E899F"}));
         }
 
@@ -31,11 +34,16 @@ namespace UE4Config.Tests
         {
             //Create a new config hierarchy, with paths to the engine as well as the project directory
             var configHierarchy = new FileConfigHierarchy(TestUtils.GetTestDataPath("MockProject"), TestUtils.GetTestDataPath("MockEngine"));
+
             //Evaluate the values and put them into a list. Should the property only have a single value, the list will have a single element.
             //Should the property not exist or should all its values have been deleted via config, the list will be empty.
-            var values = new List<string>();
-            configHierarchy.EvaluatePropertyValues("Windows", "Game", "Internationalization", "LocalizationPaths", values);
-            Assert.That(values, Is.EquivalentTo(new[]
+            var win64Values = new List<string>();
+
+            //Evaluate the property "LocalizationPaths" in the section "Internationalization" int the category "Game" for the topmost "Windows"-platform config in the hierarchy.
+            //This will take all lower hierarchy layers into account.
+            configHierarchy.EvaluatePropertyValues("Windows", "Game", "Internationalization", "LocalizationPaths", win64Values);
+
+            Assert.That(win64Values, Is.EquivalentTo(new[]
             {
                 "%GAMEDIR%Content/Localization/Game",
                 "%GAMEDIR%Content/Localization/Game/Default",
