87db936b2ff5ee5cc56c17949a3ca92c3a46da50
Patrick Michael Hopf <dragonia@gmx.at>
2022-09-12 19:05:05 +0200

    Add more sanity tests

--- UE4Config.Tests/SanityTests.cs
+++ UE4Config.Tests/SanityTests.cs
@@ -11,6 +11,13 @@ namespace UE4Config.Tests
             Assert.That("Sanity", Is.EqualTo("Sanity"));
             Assert.That(true, Is.True);
             Assert.That(false, Is.False);
+
+            Assert.That(System.Convert.ToInt32(false), Is.EqualTo(0));
+            Assert.That(System.Convert.ToInt32(true), Is.EqualTo(1));
+            Assert.That(System.Convert.ToBoolean(0), Is.False);
+            Assert.That(System.Convert.ToBoolean(1), Is.True);
+            Assert.That(!!false, Is.False);
+            Assert.That(!!true, Is.True);
         }
     }
 }
